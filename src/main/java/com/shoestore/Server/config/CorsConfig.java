55a8d267ca3957b12b8e0a373b73e1f68dd40822
Vinh <phamhuuvinh0901@gmail.com>
2025-05-30 22:57:00 +0700

    Fix: update cor config to accept from-end path

--- src/main/java/com/shoestore/Server/config/CorsConfig.java
+++ src/main/java/com/shoestore/Server/config/CorsConfig.java
@@ -12,13 +12,14 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
-    @Value("${frontend.origin.url}")
-    private String frontendUrl;
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin(frontendUrl);
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://shoe-store-frontend-kdlrbanpw-huuvinhs-projects.vercel.app"
+        ));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
         configuration.setAllowCredentials(true);
