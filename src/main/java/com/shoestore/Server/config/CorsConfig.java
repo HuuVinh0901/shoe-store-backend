ec2c773646b93ec5721728f5440cc45809b7a550
Vinh <phamhuuvinh0901@gmail.com>
2025-05-30 23:18:57 +0700

    Fix: update cors config

--- src/main/java/com/shoestore/Server/config/CorsConfig.java
+++ src/main/java/com/shoestore/Server/config/CorsConfig.java
@@ -1,11 +1,11 @@
 package com.shoestore.Server.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
@@ -19,13 +19,17 @@ public class CorsConfig {
                 "http://localhost:3000",
                 "https://shoe-store-frontend-kdlrbanpw-huuvinhs-projects.vercel.app"
         ));
-
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true); // Cho cookie
         configuration.setMaxAge(3600L);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
     }
 }
