8f28273ba555974c983301302fe5e476aac6a189
Vinh <phamhuuvinh0901@gmail.com>
2025-05-30 23:11:44 +0700

    Fix: add cors for spring security

--- src/main/java/com/shoestore/Server/config/SpringSecurityConfig.java
+++ src/main/java/com/shoestore/Server/config/SpringSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -82,8 +83,8 @@ public class SpringSecurityConfig {
 				"/api-docs/**",
 				"/",
 		};
-		http
-				.csrf(csrf -> csrf.disable())
+		http.cors() .and()
+				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(whiteList).permitAll()
 						.requestMatchers("/api/admin/**").hasRole("SUPER_ADMIN")
